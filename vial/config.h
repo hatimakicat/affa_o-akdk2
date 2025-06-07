c540e1c8d86b30b652cca9877125b36ab8928563
hatimakicat <46206108+hatimakicat@users.noreply.github.com>
2025-06-07 19:00:35 +0900

    Add files via upload

---
+++ vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0x11, 0xc9, 0x33, 0xcb, 0x31, 0x23, 0x6b, 0x1e}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
